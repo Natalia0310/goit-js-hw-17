bdb4ece0702c257fa4859c7ed834237d0c3bc8f7
Natalia0310 <ndemydenko@ukr.net>
2023-05-21 06:50:13 +0300

    new

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createGalleryCardMurkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    `;
+    })
+    .join('');
+}
+
+const cardsMarcup = createGalleryCardMurkup(galleryItems);
+galleryEl.innerHTML = cardsMarcup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
